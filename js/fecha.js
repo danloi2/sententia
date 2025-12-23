5e294b910e3a6bbc12c545275bf917bf0e27bef1
danloi2 <daniel.losada@ehu.eus>
2025-12-23 21:36:20 +0100

    refactor: Elimina comentarios innecesarios y mejora la legibilidad del código en fecha.js

--- js/fecha.js
+++ js/fecha.js
@@ -1,45 +1,37 @@
-// fecha.js
-
-// Convierte un número a romano (función auxiliar)
+// Convierte un número a romano (soporta hasta 3999)
 function numeroRomano(num) {
-    if (num <= 0) return '';
-    const numeros = [
-        ["M",1000],["CM",900],["D",500],["CD",400],
-        ["C",100],["XC",90],["L",50],["XL",40],
-        ["X",10],["IX",9],["V",5],["IV",4],["I",1]
-    ];
-    let resultado = '';
-    for (const [romano, valor] of numeros) {
-        while (num >= valor) {
-            resultado += romano;
-            num -= valor;
-        }
+  if (num === 0) return 'N'; // cero en romano
+  const valores = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const simbolos = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  let resultado = '';
+  num = Math.abs(num); // para años negativos
+  for (let i = 0; i < valores.length; i++) {
+    while (num >= valores[i]) {
+      resultado += simbolos[i];
+      num -= valores[i];
     }
-    return resultado;
+  }
+  return resultado;
 }
 
 // Genera la fecha en latín
 function fechaHoyLatina() {
-    const dias_semana_latinos = [
-        'Feria Prima',
-        'Feria Secunda',
-        'Feria Tertia',
-        'Feria Quarta',
-        'Feria Quinta',
-        'Feria Sexta',
-        'Feria Septima'
-    ];
-    const meses_latinos = {
-        1: 'Ianuarii', 2: 'Februarii', 3: 'Martii', 4: 'Aprilis',
-        5: 'Maii', 6: 'Iunii', 7: 'Iulii', 8: 'Augusti',
-        9: 'Septembris', 10: 'Octobris', 11: 'Novembris', 12: 'Decembris'
-    };
+  const dias_semana_latinos = [
+    'Feria Prima', 'Feria Secunda', 'Feria Tertia',
+    'Feria Quarta', 'Feria Quinta', 'Feria Sexta', 'Feria Septima'
+  ];
 
-    const fecha = new Date();
-    const dia_semana = fecha.getDay(); // 0-6
-    const dia_mes = numeroRomano(fecha.getDate());
-    const mes = meses_latinos[fecha.getMonth() + 1];
-    const anio = numeroRomano(fecha.getFullYear());
+  const meses_latinos = [
+    '', 'Ianuarii', 'Februarii', 'Martii', 'Aprilis',
+    'Maii', 'Iunii', 'Iulii', 'Augusti',
+    'Septembris', 'Octobris', 'Novembris', 'Decembris'
+  ];
 
-    return `${dias_semana_latinos[dia_semana]}, die ${dia_mes} mensis ${mes} ${anio}`;
+  const fecha = new Date();
+  const diaSemana = fecha.getDay();
+  const diaMes = numeroRomano(fecha.getDate());
+  const mes = meses_latinos[fecha.getMonth() + 1]; // getMonth() 0-11
+  const anio = numeroRomano(fecha.getFullYear());
+
+  return `${dias_semana_latinos[diaSemana]}, die ${diaMes} mensis ${mes} ${anio}`;
 }
