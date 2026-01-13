af63f1acc9928a71fd8f7711ff8d7c638a28c72d
Daniel Losada <daniel.losada@ehu.eus>
2026-01-13 13:53:37 +0100

    Delete src-tauri directory

--- src-tauri/src/main.rs
+++
@@ -1,7 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    // Esto llama a la función run() que está en tu archivo lib.rs
-    sententiadiei_lib::run()
-}
